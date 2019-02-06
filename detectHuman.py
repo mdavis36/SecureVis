bd51cbd43cdd6e9c84c5c66c9a11f57c4ab4ef93
mdavis36 <m_davis23@u.pacific.edu>
2019-02-06 15:19:01 -0800

    Altering threshold

--- detectHuman.py
+++ detectHuman.py
@@ -6,7 +6,7 @@ import time
 opt = {
     'model':'cfg/yolov1.cfg',
     'load':'bin/yolov1.weights',
-    'threshold':0.01,
+    'threshold':0.02,
     'gpu':1.0
 }
 
@@ -14,7 +14,9 @@ tfnet = TFNet(opt)
 
 cap = cv2.VideoCapture('test480.mp4')
 cols = [tuple(255 * np.random.rand(3)) for i in range(5)]
-
+out = cv2.VideoWriter('out.avi', cv2.VideoWriter_fourcc('M','J','P','G'), cap.get(cv2.CAP_PROP_FPS),\
+                                    (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))\
+                     )
 
 frame_count = 0
 while (cap.isOpened()):
@@ -22,7 +24,7 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     frame_count += 1
     if ret:
-        if frame_count%3 == 0:
+        if frame_count%1 == 0:
             res = tfnet.return_predict(frame)
             for c, r in zip(cols, res):
                 tl = (r['topleft']['x'], r['topleft']['y'])
@@ -32,10 +34,12 @@ while (cap.isOpened()):
                     frame = cv2.rectangle(frame, tl, br, c, 7)
                     frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 2)
         cv2.imshow('frame', frame)
+        out.write(frame)
         print('FPS {:.1f}'.format(1/(time.time() - stime)))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         cap.release()
+        out.release()
         cv2.destroyAllWindows()
         break
