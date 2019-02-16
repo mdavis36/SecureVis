380cc12885867b6fb6bfce11d01adb08ce9dbb39
dsamuel27 <david.samuel1227@gmail.com>
2019-02-15 21:36:25 -0800

    Cleaned up server.py. Server can accept multiple clients one at a time

--- server.py
+++ server.py
@@ -6,41 +6,57 @@ import pickle
 import numpy as np
 import struct
 import sys
+import thread
+import datetime
 
 HOST ='127.0.0.1'
 PORT = 1110
 BUFFER_SIZE = 4096
 STRUCT_ARG = "L"
 
+
+def new_client(conn,addr):
+    now = datetime.datetime.now()
+    dateAndTime = now.strftime("%Y-%m-%d_%H-%M")
+    data = ""
+    # Packet will be the size of largest unsigned LONG value
+    packet_size = struct.calcsize(STRUCT_ARG);
+    #out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+    while True:
+        # get the size of the data being sent over
+        while len(data) < packet_size:
+            data += conn.recv(BUFFER_SIZE)
+        packed_msg_size = data[:packet_size]
+        data = data[packet_size:]
+        msg_size = struct.unpack(STRUCT_ARG,packed_msg_size)[0]
+        # get the data from the struct that refers to the video.
+        while len(data) < msg_size:
+            data += conn.recv(BUFFER_SIZE)
+        raw_frame = data[:msg_size]
+        data = data[msg_size:]
+
+        frame = pickle.loads(raw_frame)
+        #print frame
+        cv2.imshow('frame',frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            conn.close()
+            break
+
+
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((HOST,PORT))
 s.listen(10)
 print 'Socket is now listening'
 
-conn, addr = s.accept()
 
-data = ""
-# Packet will be the size of largest unsigned LONG value
-packet_size = struct.calcsize(STRUCT_ARG);
+
 
 while True:
-    while len(data) < packet_size:
-        data += conn.recv(BUFFER_SIZE)
-    packed_msg_size = data[:packet_size]
-    data = data[packet_size:]
-    msg_size = struct.unpack(STRUCT_ARG,packed_msg_size)[0]
-    while len(data) < msg_size:
-        data += conn.recv(BUFFER_SIZE)
-    raw_frame = data[:msg_size]
-    data = data[msg_size:]
+    conn, addr = s.accept()
+    new_client(conn,addr)
 
-    frame = pickle.loads(raw_frame)
-    print frame
-    cv2.imshow('frame',frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
+s.close()
 
 
