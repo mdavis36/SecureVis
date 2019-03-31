b71e1a6df9ab00f6995e23139bbe28cf935ad10b
dsamuel27 <david.samuel1227@gmail.com>
2019-03-31 11:13:42 -0700

    Added some comments for socket logic

--- src/Communication.java
+++ src/Communication.java
@@ -1,11 +1,35 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
+
+// initialize GUI, restore from state
+
+// start server while loop
+	// recieve and send data to master
+	// update GUI accordingly
+	// if user wants to exit settings page terminate
 
 
 public class Communication {
+	
 	private Socket socket;
+	
+	
 	private static final int PORT = 65432;
+	private static final int HEADER_SIZE = 16;
+	
 	Communication() throws UnknownHostException, IOException {
 		socket = new Socket(InetAddress.getLocalHost(),PORT);
 	}
+	/*
+	public String getInput() {
+		try {
+			Scanner in = new Scanner(socket.getInputStream());
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	} */
 }
