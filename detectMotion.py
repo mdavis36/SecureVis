3239c0671765ca4be07094c3ed99010affcfc56b
mdavis36 <m_davis23@u.pacific.edu>
2019-04-02 14:13:34 -0700

    Detect Motion simulating three cameras.

--- detectMotion.py
+++ detectMotion.py
@@ -18,16 +18,23 @@ while(1):
     frameCount += 1
     resizedFrame = cv2.resize(frame, (0, 0), fx=0.50, fy=0.50)
 
-    fgmask = fgbg.apply(frame)
+    frame1 = resizedFrame
+    frame2 = resizedFrame
+
+    fgmask = fgbg.apply(resizedFrame)
+    fgmask1 = fgbg.apply(frame1)
+    fgmask2 = fgbg.apply(frame2)
 
     count = np.count_nonzero(fgmask)
 
     print('Frame: %d, Pixel Count: %d' % (frameCount, count))
-    if (count > 5000):
+    if (count > 1000):
         cv2.putText(frame, 'Motion Detected!', (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
 
     cv2.imshow('Feed', frame)
     cv2.imshow('Mask', fgmask)
+    cv2.imshow('Mask1', fgmask1)
+    cv2.imshow('Mask2', fgmask2)
 
     k = cv2.waitKey(1) & 0xff
     if k ==27:
