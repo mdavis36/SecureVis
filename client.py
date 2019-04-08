5d3951b821d0eb0a8b752fb8c4db6b5b5ca03079
mdavis36 <m_davis23@u.pacific.edu>
2019-04-08 14:41:28 -0700

    client streams video between edge and master

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 import socket
 import cv2
 import numpy as np
-import pickle
+import cPickle as pickle
 import struct
 import sys
 
@@ -11,13 +11,15 @@ import sys
 HOST ='192.168.0.24'
 PORT = 1110
 VIDFILE_NAME = "testFootage/test480.mp4"
-ROOM_NAME = bytes(sys.argv[1], 'utf-8')
+STRUCT_ARG = "I"
 
+#ROOM_NAME = bytes(sys.argv[1], 'utf-8')
+ROOM_NAME = str(sys.argv[1])
 
 # Set up socket and connect to server.
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((HOST,int(sys.argv[2])))
-s.sendall(ROOM_NAME);
+s.sendall(str.encode(ROOM_NAME));
 
 
 # Send frame Data
@@ -25,14 +27,19 @@ cap = cv2.VideoCapture(VIDFILE_NAME)
 ret,frame = cap.read()
 while ret:
     #ret,frame = cap.read()
-    data = pickle.dumps(frame)
-    
+    frame = cv2.resize(frame, (320, 240))
+    data = pickle.dumps(frame, protocol = 2)
+    print("Data len : ", len(str(data)))
+#try:
+    #want to send the whole buffer at once
     try:
-        #want to send the whole buffer at once
-        s.sendall(struct.pack('L', len(data))+data)
-        #print 'I AM HERE'
+        s.sendall(struct.pack(STRUCT_ARG, len(data))+data)
+        print("Sending Frame")
+        cv2.imshow('sending', frame) 
     except:
         print ("SENDING FAILED")
+#iexcept:
+    #print ("SENDING FAILED")
     ret,frame = cap.read()
 
 cv2.destroyAllWindows()
