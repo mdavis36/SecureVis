51bdd065bda7b6d81470686e85111a7ab5752ab8
mdavis36 <m_davis23@u.pacific.edu>
2019-04-08 16:12:06 -0700

    client streams data when motion detected.

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 #/usr/bin/python
 
+import atexit
 import socket
 import cv2
 import numpy as np
@@ -7,39 +8,49 @@ import cPickle as pickle
 import struct
 import sys
 
-
 HOST ='192.168.0.24'
 PORT = 1110
 VIDFILE_NAME = "testFootage/test480.mp4"
 STRUCT_ARG = "I"
-
-#ROOM_NAME = bytes(sys.argv[1], 'utf-8')
 ROOM_NAME = str(sys.argv[1])
+
+
+def exit_handler(s, cap):
+    print("Closing connection and camera(s)")
+    s.close()
+    cv2.destroyAllWindows()
+    cap.release()
+
+#------------------MAIN EXECUTION------------------
 
 # Set up socket and connect to server.
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((HOST,int(sys.argv[2])))
 s.sendall(str.encode(ROOM_NAME));
 
+fgbg = cv2.createBackgroundSubtractorMOG2(100, 16, False)
 
 # Send frame Data
-cap = cv2.VideoCapture(VIDFILE_NAME)
+cap = cv2.VideoCapture(0)#VIDFILE_NAME)
 ret,frame = cap.read()
+
+# Handle exit sequence
+atexit.register(exit_handler, s, cap)
+
+
 while ret:
     #ret,frame = cap.read()
     frame = cv2.resize(frame, (320, 240))
+    fgmask = fgbg.apply(frame)
+    count = np.count_nonzero(fgmask)
+
     data = pickle.dumps(frame, protocol = 2)
-    print("Data len : ", len(str(data)))
-#try:
-    #want to send the whole buffer at once
-    try:
-        s.sendall(struct.pack(STRUCT_ARG, len(data))+data)
-        print("Sending Frame")
-        cv2.imshow('sending', frame) 
-    except:
-        print ("SENDING FAILED")
-#iexcept:
-    #print ("SENDING FAILED")
+
+    if (count > 1000):
+        #want to send the whole buffer at once
+        try:
+            s.sendall(struct.pack(STRUCT_ARG, len(data))+data)
+        except:
+            print ("SENDING FAILED")
     ret,frame = cap.read()
 
-cv2.destroyAllWindows()
